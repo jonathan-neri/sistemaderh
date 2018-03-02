79fd5b5bfbdbf4fed0a087bd8354430db577a4bb
Jonathan Neri <jonathanlucenaneri@gmail.com>
2018-03-01 22:04:27 -0300

    Testando a primeira classe de dominio - Pessoa

--- src/main/java/com/jonathanneri/sistemaderh/resources/PessoaResources.java
+++ src/main/java/com/jonathanneri/sistemaderh/resources/PessoaResources.java
@@ -1,16 +1,29 @@
 package com.jonathanneri.sistemaderh.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jonathanneri.sistemaderh.domain.Pessoa;
 
 @RestController
 @RequestMapping(value="/pessoa")
 public class PessoaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Pessoa> listar() {
+		
+		Pessoa pes1 = new Pessoa(1, "jonathan");
+		Pessoa pes2 = new Pessoa(2, "TI");
+				
+		List<Pessoa> lista = new ArrayList<>();		
+		lista.add(pes1);
+		lista.add(pes2);
+		
+		return lista;
 	}
 
 }
